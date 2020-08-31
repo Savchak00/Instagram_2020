2e8d20359de8c89c5b2a6db3f4d25e87f21348d6
Danylo Savchak <daniel.savchak@gmail.com>
2020-08-31 19:26:40 +0300

    Create user interface in LoginVC

---
+++ Instagram/Resourses/Extensions.swift
@@ -0,0 +1,36 @@
+//
+//  Extensions.swift
+//  Instagram
+//
+//  Created by Daniel Savchak on 31.08.2020.
+//  Copyright © 2020 Danylo Savchak. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    public var width: CGFloat {
+        return frame.size.width
+    }
+    
+    public var height: CGFloat {
+        return frame.size.height
+    }
+    
+    public var top: CGFloat {
+        return frame.origin.y
+    }
+    
+    public var bottom: CGFloat {
+        return frame.origin.y + frame.size.height
+    }
+    
+    public var left: CGFloat {
+        return frame.origin.x
+    }
+    
+    public var rigth: CGFloat {
+        return frame.origin.x + frame.size.width
+    }
+}
