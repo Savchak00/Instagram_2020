3f709325948085d0866d6c3219da04772af399e9
Danylo Savchak <daniel.savchak@gmail.com>
2020-08-31 21:39:10 +0300

    Create RegistrationVC and functions in databaseManager and AuthManager now you can create user in database

--- Instagram/Controllers/Other/Unboarding/LoginViewController.swift
+++ Instagram/Controllers/Other/Unboarding/LoginViewController.swift
@@ -206,6 +206,31 @@ class LoginViewController: UIViewController {
         }
         
         // log in functionality
+        
+        var username: String?
+        var email: String?
+        
+        if usernameEmail.contains("@"), usernameEmail.contains(".") {
+            // email
+            email = usernameEmail
+        } else {
+            // username
+            username = usernameEmail
+        }
+        
+        AuthManager.shared.loginUser(username: username, email: email, password: password) { success in
+            DispatchQueue.main.async { // because closure - in background thread
+                if success {
+                    // user logged in
+                    self.dismiss(animated: true, completion: nil)
+                } else {
+                    // error occured
+                    let alert = UIAlertController(title: "Log In Error", message: "We were unable to log you in.", preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
+                    self.present(alert,animated: true)
+                }
+            }
+        }
     }
     @objc private func didTapTermsButton() {
         guard let url = URL(string: "https://help.instagram.com/478745558852511/?helpref=hc_fnav") else {
@@ -223,7 +248,8 @@ class LoginViewController: UIViewController {
     }
     @objc private func didTapCreateAccountButton() {
         let vc = RegistrationViewController()
-        present(vc,animated: true)
+        vc.title = "Create Account"
+        present(UINavigationController(rootViewController: vc),animated: true)
     }
 
 }
