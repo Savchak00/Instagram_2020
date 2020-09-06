51d614eb3fccb9ca4d294f85c1d1b16584e9fc46
Danylo Savchak <daniel.savchak@gmail.com>
2020-09-06 20:19:40 +0300

    upgrade ProfileVC

--- Instagram/Controllers/Core Tabs/ProfileViewController.swift
+++ Instagram/Controllers/Core Tabs/ProfileViewController.swift
@@ -13,6 +13,8 @@ final class ProfileViewController: UIViewController {
 
     private var collectionView: UICollectionView?
     
+    private var userPosts = [UserPost]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
@@ -29,7 +31,6 @@ final class ProfileViewController: UIViewController {
         collectionView?.dataSource = self
         collectionView = UICollectionView(frame: .zero,
                                           collectionViewLayout: layout)
-        collectionView?.backgroundColor = .red
         
         // Cells
         collectionView?.register(PhotoCollectionViewCell.self, forCellWithReuseIdentifier: PhotoCollectionViewCell.identifier)
@@ -76,18 +77,27 @@ extension ProfileViewController: UICollectionViewDelegate, UICollectionViewDataS
         if section == 0 {
             return 0
         }
-        
+//        return userPosts.count
         return 30
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let model = userPosts[indexPath.row]
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PhotoCollectionViewCell.identifier , for: indexPath) as! PhotoCollectionViewCell
         cell.configure(debug: "test")
+//        cell.configure(with: model)
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         collectionView.deselectItem(at: indexPath, animated: true)
+        
+        // get the model and open PostViewController
+//        let model = userPosts[indexPath.row]
+        let vc = PostViewController(model: nil)
+        vc.title = "Post"
+        vc.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.pushViewController(vc, animated: true)
     }
     
     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
@@ -105,6 +115,7 @@ extension ProfileViewController: UICollectionViewDelegate, UICollectionViewDataS
         }
         
         let profileHeader = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: ProfileInfoHeaderCollectionReusableView.identifier, for: indexPath) as!  ProfileInfoHeaderCollectionReusableView
+        profileHeader.delegate = self
         
         return profileHeader
     }
@@ -116,5 +127,35 @@ extension ProfileViewController: UICollectionViewDelegate, UICollectionViewDataS
         // Size of section tabs
         return CGSize(width: collectionView.width, height: 65)
     }
+    
+}
+
+// MARK: - ProfileInfoHeaderCollectionReusableViewDelegate
+extension ProfileViewController: ProfileInfoHeaderCollectionReusableViewDelegate {
+    func profileHeaderDidTapPostsButton(_ header: ProfileInfoHeaderCollectionReusableView) {
+        // scroll to the posts section
+        collectionView?.scrollToItem(at: IndexPath(row: 0, section: 1), at: .top, animated: true)
+    }
+    
+    func profileHeaderDidTapFollowersButton(_ header: ProfileInfoHeaderCollectionReusableView) {
+        let vc = ListViewController()
+        vc.title = "Followers"
+        vc.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    func profileHeaderDidTapFollowingButton(_ header: ProfileInfoHeaderCollectionReusableView) {
+        let vc = ListViewController()
+        vc.title = "Following"
+        vc.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    func profileHeaderDidTapEditProfileButtonButton(_ header: ProfileInfoHeaderCollectionReusableView) {
+        let vc = EditProfileViewController()
+        vc.title = "Edit Profile"
+        present(UINavigationController(rootViewController: vc),animated: true)
+    }
+    
     
 }
--- Instagram/Views/CollectionView Headers/ProfileInfoHeaderCollectionReusableView.swift
+++ Instagram/Views/CollectionView Headers/ProfileInfoHeaderCollectionReusableView.swift
@@ -8,19 +8,172 @@
 
 import UIKit
 
-class ProfileInfoHeaderCollectionReusableView: UICollectionReusableView {
+protocol ProfileInfoHeaderCollectionReusableViewDelegate: AnyObject {
+    func profileHeaderDidTapPostsButton(_ header: ProfileInfoHeaderCollectionReusableView)
+    func profileHeaderDidTapFollowersButton(_ header: ProfileInfoHeaderCollectionReusableView)
+    func profileHeaderDidTapFollowingButton(_ header: ProfileInfoHeaderCollectionReusableView)
+    func profileHeaderDidTapEditProfileButtonButton(_ header: ProfileInfoHeaderCollectionReusableView)
+
+}
+
+final class ProfileInfoHeaderCollectionReusableView: UICollectionReusableView {
     
     static let identifier = "ProfileInfoHeaderCollectionReusableView"
     
+    public weak var delegate: ProfileInfoHeaderCollectionReusableViewDelegate?
+    
+    private let profilePhotoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .red
+        imageView.layer.masksToBounds = true
+        return imageView
+    }()
+    
+    private let postButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Posts", for: .normal)
+        button.setTitleColor(.label, for: .normal)
+        button.backgroundColor = .secondarySystemBackground
+        return button
+    }()
+    
+    private let followingButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Followings", for: .normal)
+        button.setTitleColor(.label, for: .normal)
+        button.backgroundColor = .secondarySystemBackground
+        return button
+    }()
+    
+    private let followersButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Followers", for: .normal)
+        button.setTitleColor(.label, for: .normal)
+        button.backgroundColor = .secondarySystemBackground
+        return button
+    }()
+    
+    private let editProfileButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Edit your profile", for: .normal)
+        button.setTitleColor(.label, for: .normal)
+        button.backgroundColor = .secondarySystemBackground
+        return button
+    }()
+    
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Joe Smith"
+        label.textColor = .label
+        label.numberOfLines = 1
+        return label
+    }()
+    
+    private let bioLabel: UILabel = {
+        let label = UILabel()
+        label.text = "This is the first account!"
+        label.textColor = .label
+        label.numberOfLines = 0 // line wrap
+        return label
+    }()
+    
+    //MARK: - Init
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .systemBlue
+        addSubviews()
+        addButtonActions()
+        backgroundColor = .systemBackground
         clipsToBounds = true
         
     }
     
+    private func addSubviews() {
+        addSubview(profilePhotoImageView)
+        addSubview(followersButton)
+        addSubview(followingButton)
+        addSubview(postButton)
+        addSubview(nameLabel)
+        addSubview(bioLabel)
+        addSubview(editProfileButton)
+    }
+    
+    private func addButtonActions() {
+        followersButton.addTarget(self, action: #selector(didTapFollowersButton), for: .touchUpInside)
+        followingButton.addTarget(self, action: #selector(didTapFollowingsButton), for: .touchUpInside)
+        postButton.addTarget(self, action: #selector(didTapPostButton), for: .touchUpInside)
+        editProfileButton.addTarget(self, action: #selector(didTapEditProfileButton), for: .touchUpInside)
+
+    }
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        let profilePhotoSize = width/4
+        profilePhotoImageView.frame = CGRect(x: 5,
+                                             y: 5,
+                                             width: profilePhotoSize,
+                                             height: profilePhotoSize).integral
+        
+        profilePhotoImageView.layer.cornerRadius = profilePhotoSize/2.0
+        
+        let buttonHeight = profilePhotoSize/2
+        let countButtonWidth = (width - 10 - profilePhotoSize)/3
+        
+        postButton.frame = CGRect(x: profilePhotoImageView.rigth,
+                                  y: 5,
+                                  width: countButtonWidth,
+                                  height: buttonHeight).integral
+        
+        followersButton.frame = CGRect(x: postButton.rigth,
+                                       y: 5,
+                                       width: countButtonWidth,
+                                       height: buttonHeight).integral
+        
+        followingButton.frame = CGRect(x: followersButton.rigth,
+                                       y: 5,
+                                       width: countButtonWidth,
+                                       height: buttonHeight).integral
+        
+        editProfileButton.frame = CGRect(x: profilePhotoImageView.rigth,
+                                         y: 5 + buttonHeight,
+                                         width: countButtonWidth * 3,
+                                         height: buttonHeight).integral
+        
+        nameLabel.frame = CGRect(x: 5,
+                                 y: 5 + profilePhotoImageView.bottom,
+                                 width: width-10,
+                                 height: 50).integral
+        
+        let bioLabelSize = bioLabel.sizeThatFits(frame.size)
+        
+        nameLabel.frame = CGRect(x: 5,
+                                 y: 5 + nameLabel.bottom,
+                                 width: width-10,
+                                 height: bioLabelSize.height ).integral
+
+    }
+    
+    // MARK: - Actions
+    
+    @objc private func didTapFollowersButton() {
+        delegate?.profileHeaderDidTapFollowersButton(self)
+    }
+    
+    @objc private func didTapFollowingsButton() {
+        delegate?.profileHeaderDidTapFollowingButton(self)
+    }
+    
+    @objc private func didTapPostButton() {
+        delegate?.profileHeaderDidTapPostsButton(self)
+    }
+    
+    @objc private func didTapEditProfileButton() {
+        delegate?.profileHeaderDidTapEditProfileButtonButton(self)
     }
     
 }
