a1bcb263bc000768834b5b2c9019b9d6c1673655
Danylo Savchak <daniel.savchak@gmail.com>
2020-10-07 17:29:39 +0300

    To ProfileVC add grid/tag buttons & start creating ListVC for Followes and Following butoons

---
+++ Instagram/Views/Cells/UserFollowTableViewCell.swift
@@ -0,0 +1,77 @@
+//
+//  UserFollowTableViewCell.swift
+//  Instagram
+//
+//  Created by Daniel Savchak on 07.10.2020.
+//  Copyright © 2020 Danylo Savchak. All rights reserved.
+//
+
+import UIKit
+
+protocol UserFollowTableViewCellDelegate: AnyObject { // AnyObject for beeing a weak property
+    func didTapFollowUnFollowButton(model: String)
+}
+
+class UserFollowTableViewCell: UITableViewCell {
+    static let identifier  = "UserFollowTableViewCell"
+    
+    weak var delegate: UserFollowTableViewCellDelegate?
+    
+    private let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 1
+        label.font = .systemFont(ofSize: 17, weight: .semibold)
+        return label
+    }()
+    
+    private let usernameLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 1
+        label.font = .systemFont(ofSize: 16, weight: .regular)
+        return label
+    }()
+    
+    private let followButton: UIButton = {
+        let button = UIButton()
+        return button
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        contentView.clipsToBounds = true
+        addSubview(nameLabel)
+        addSubview(usernameLabel)
+        addSubview(followButton)
+        addSubview(profileImageView)
+    }
+    
+    public func configure(with model: String ) {
+        
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        profileImageView.image = nil
+        nameLabel.text = nil
+        usernameLabel.text = nil
+        followButton.setTitle(nil, for: .normal)
+        followButton.layer.borderWidth = 0
+        followButton.backgroundColor = nil
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
